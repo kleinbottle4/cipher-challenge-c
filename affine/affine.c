c3662d32a8ef960987f2b571ae943b21868b9bce
syed343 <geoffgumtree343@gmail.com>
2019-10-24 09:04:20 +0100

    affine.c works

--- affine/affine.c
+++ affine/affine.c
@@ -19,32 +19,45 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "affine.h"
+
+#define ALPHABET_LEN 26
+#define KW_LEN ALPHABET_LEN + 1
 
 int main(int argc, const char **argv)
 {
-    int msg_len, kw_len;
-    int a, b;
-    
-    printf("Message length: ");
-    scanf("%d", &msg_len);
-    char msg[msg_len];
-    printf("Message: ");
-    fgets(msg, msg_len, stdin);
+	int msg_len;
+	if (argc >= 2)
+		msg_len = atoi(argv[1]);
+	else
+		msg_len = 500;
+	
+	char msg[msg_len + 1], kw[KW_LEN];
+	int a, b;
+	
+	printf("# Messages are a maximum of %d characters long.\n", msg_len);
+	puts("# Keywords are a maximum of 26 characters long.");
+	puts("# Strings longer than their maximum length are truncated.");
+    putchar('\n');
 
-    printf("Keyword length: ");
-    scanf("%d", &kw_len);
-    char kw[kw_len];
-    printf("Keyword: ");
-    fgets(kw, kw_len, stdin);
+	while (true) {
+		printf("# Message:\n");
+		fgets(msg, msg_len, stdin);	
 
-    printf("a: ");
-    scanf("%d", &a);
-    printf("b: ");
-    scanf("%d", &b);
+		printf("# Keyword:\n");
+		fgets(kw, KW_LEN, stdin);
 
-    affine_str(msg, a, b, ALPHABET, true);
-    puts(msg);
+		printf("# a: ");
+		scanf("%d", &a);
+		printf("# b: ");
+		scanf("%d", &b);
 
+		affine_str(msg, a, b, ALPHABET, true);
+		puts(msg);
+		
+		getchar();
+	}
+	
     return 0;
 }
